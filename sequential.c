2681f4c3ba952da93fb93b8d76e2d663c21b8bf9
KardaChalak <karda.chalak@gmail.com>
2016-03-28 16:44:41 +0200

    Initation of grid boundary

--- sequential.c
+++ sequential.c
@@ -18,21 +18,30 @@
 	
 	int gridSize, numIters, i;
 	
-	/*Andreas I interpret the grid size as the length of the side.
+	/*Andreas! I interpret the grid size as the length of the side.
 	  Otherwise we have to take the sqrt of it to get the sides when
 	  malloc'ing, change it if you see it fitting. :) */
-	gridSize = (argc > 1)? atoi(argv[1]) : GRIDSIZE;
+	gridSize = (argc > 1)? atoi(argv[1]) + 2 : GRIDSIZE;
 	numIters = (argc > 2)? atoi(argv[2]) : NUMITERS;
 	
-	printf("gridSize: %d\n", gridSize);
-	printf("numIters: %d\n", numIters);
+	//printf("gridSize: %d\n", gridSize);
+	//printf("numIters: %d\n", numIters);
 	
 	/*malloc the columns*/
-	int **grid = (int **) malloc(gridSize+2 * sizeof(int*));
-	int **new = (int **) malloc(gridSize+2 * sizeof(int*));
+	int **grid = (int **) malloc(gridSize * sizeof(int*));
+	int **new = (int **) malloc(gridSize * sizeof(int*));
 	/*malloc the rows*/
-	for(i = 0; i < gridSize+2; i++){
-		grid[i] = (int *) malloc(gridSize+2 * sizeof(int));
-		new[i] = (int *) malloc(gridSize+2 * sizeof(int));
+	for(i = 0; i < gridSize; i++){
+		grid[i] = (int *) malloc(gridSize * sizeof(int));
+		new[i] = (int *) malloc(gridSize * sizeof(int));
 	}
+	/*Initiation of grid boundary */
+	for(i = 0; i < gridSize; i++){
+		grid[0][i] = 1;				// upper boundary
+		grid[gridSize-1][i] = 1;	// lower boundary
+		grid[i][0] = 1; 			// left  boundary
+		grid[i][gridSize-1] =1;		// right boundary
+	}
+	for(i = 1)
+	
 }
