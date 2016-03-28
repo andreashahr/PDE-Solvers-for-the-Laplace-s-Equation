2aa0621ee6f3f4bdd6721220fb570e0b86bfa1eb
KardaChalak <karda.chalak@gmail.com>
2016-03-28 19:54:50 +0200

    initiation of grid and print of the grid has been done

--- sequential.c
+++ sequential.c
@@ -16,7 +16,7 @@
 	  numIters, the number of iterations to use
 	 */
 	
-	int gridSize, numIters, i;
+	int gridSize, numIters, i, j;
 	
 	/*Andreas! I interpret the grid size as the length of the side.
 	  Otherwise we have to take the sqrt of it to get the sides when
@@ -24,8 +24,8 @@
 	gridSize = (argc > 1)? atoi(argv[1]) + 2 : GRIDSIZE;
 	numIters = (argc > 2)? atoi(argv[2]) : NUMITERS;
 	
-	//printf("gridSize: %d\n", gridSize);
-	//printf("numIters: %d\n", numIters);
+	printf("gridSize: %d\n", gridSize);
+	printf("numIters: %d\n", numIters);
 	
 	/*malloc the columns*/
 	int **grid = (int **) malloc(gridSize * sizeof(int*));
@@ -36,12 +36,31 @@
 		new[i] = (int *) malloc(gridSize * sizeof(int));
 	}
 	/*Initiation of grid boundary */
+	/*Hörnen initieras 2 gånger. Går säkert att göra bättre men just nu känns det
+	  inte värt*/
 	for(i = 0; i < gridSize; i++){
 		grid[0][i] = 1;				// upper boundary
+		new[0][i] =1;
 		grid[gridSize-1][i] = 1;	// lower boundary
+		new[gridSize-1][i] = 1;
 		grid[i][0] = 1; 			// left  boundary
+		new[i][0] = 1;
 		grid[i][gridSize-1] =1;		// right boundary
+		new[i][gridSize-1] = 1;
 	}
-	for(i = 1)
+	/*Instantiation of grids */
+	for(i = 1; i < gridSize-1; i++){
+		for(j = 1; j < gridSize-1; j++){
+			grid[i][j] = 0;
+			//new[i][j] = 0; // behövs nog inte
+		}	
+	}
 	
+	/*Prints the grid with boundary*/
+	for(i = 0; i < gridSize; i++){
+			printf("\n");
+		for(j = 0; j < gridSize; j++){
+			printf("%d  ", grid[i][j]);
+		}	
+	}
 }
