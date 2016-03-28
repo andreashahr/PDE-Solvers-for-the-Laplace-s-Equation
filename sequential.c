aab36b40856eeef4d19a0fc0feb4b7091161b8ee
KardaChalak <karda.chalak@gmail.com>
2016-03-28 10:10:44 +0200

    mallocing for extended grid

--- sequential.c
+++ sequential.c
@@ -16,12 +16,23 @@
 	  numIters, the number of iterations to use
 	 */
 	
-	int gridSize;
-	int numIters;
+	int gridSize, numIters, i;
 	
+	/*Andreas I interpret the grid size as the length of the side.
+	  Otherwise we have to take the sqrt of it to get the sides when
+	  malloc'ing, change it if you see it fitting. :) */
 	gridSize = (argc > 1)? atoi(argv[1]) : GRIDSIZE;
 	numIters = (argc > 2)? atoi(argv[2]) : NUMITERS;
 	
 	printf("gridSize: %d\n", gridSize);
 	printf("numIters: %d\n", numIters);
+	
+	/*malloc the columns*/
+	int **grid = (int **) malloc(gridSize+2 * sizeof(int*));
+	int **new = (int **) malloc(gridSize+2 * sizeof(int*));
+	/*malloc the rows*/
+	for(i = 0; i < gridSize+2; i++){
+		grid[i] = (int *) malloc(gridSize+2 * sizeof(int));
+		new[i] = (int *) malloc(gridSize+2 * sizeof(int));
+	}
 }
