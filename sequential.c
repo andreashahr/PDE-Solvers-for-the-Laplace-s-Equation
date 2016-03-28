8527c908b58bb098a19295eb4c175770a9ecd7c0
KardaChalak <karda.chalak@gmail.com>
2016-03-28 09:49:06 +0200

    Starting input handler

--- sequential.c
+++ sequential.c
@@ -4,7 +4,24 @@
 	
 	A sequential Jacobi iteration program
 */
+ #include <stdlib.h>
+ #include <stdio.h>
  
- int main (int argv, char *argv[]){
+ #define GRIDSIZE 10
+ #define NUMITERS 10
  
+ int main(int argc, char *argv[]){
+ 
+	/*gridSize, the grid size not including boundaries
+	  numIters, the number of iterations to use
+	 */
+	
+	int gridSize;
+	int numIters;
+	
+	gridSize = (argc > 1)? atoi(argv[1]) : GRIDSIZE;
+	numIters = (argc > 2)? atoi(argv[2]) : NUMITERS;
+	
+	printf("gridSize: %d\n", gridSize);
+	printf("numIters: %d\n", numIters);
 }
