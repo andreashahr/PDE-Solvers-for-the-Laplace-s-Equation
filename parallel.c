b0949d8e4947e1cbe707a0dc15c4935f68b04ac2
KardaChalak <karda.chalak@gmail.com>
2016-04-19 23:07:42 +0200

    parallelized jacobi iterations

--- parallel.c
+++ parallel.c
@@ -21,6 +21,7 @@
  #define GRIDSIZE 10
  #define NUMITERS 10
 
+ 
 int main(int argc, char *argv[]){
 
 	/*gridSize, the grid size not including boundaries
@@ -51,14 +52,82 @@ int main(int argc, char *argv[]){
 		new[i] = (double *) malloc(gridSize * sizeof(double));
 	}
 	
-	void barrier(int id){
-		// stub function for barrier
+	/*Initiation of grid boundary */
+	for(i = 0; i < gridSize; i++){
+		grid[0][i] = 1;				// upper boundary
+		new[0][i] = 1;
+		grid[gridSize-1][i] = 1;	// lower boundary
+		new[gridSize-1][i] = 1;
+		grid[i][0] = 1; 			// left  boundary
+		new[i][0] = 1;
+		grid[i][gridSize-1] = 1;	// right boundary
+		new[i][gridSize-1] = 1;
 	}
 	
+	/*Instantiation of grids */
+	for(i = 1; i < gridSize-1; i++){
+		for(j = 1; j < gridSize-1; j++){
+			grid[i][j] = 0;
+		}	
+	}
 	
+	/*Rows that will be processed by each processor*/
 	
+	workLoad = gridSize / numProc;
+	
+	/*Vi bör eventuellt använda shift istället för * 0.25*/
+	/*Jacobi iterations*/
+	
+	for(k = 0; k < numIters; k++) {
+		
+		#pragma omp parallel for schedule(static, workLoad) private(j)
+		for(i = 1; i < gridSize-1; i++) {
+			for(j = 1; j < gridSize-1; j++) {
+				new[i][j] = (grid[i-1][j] +
+							 grid[i+1][j] +
+							 grid[i][j-1] +
+							 grid[i][j+1]) * 0.25;
+			}
+		}
+		/*implicit barrier (it is needed)*/
+		#pragma omp parallel for schedule(static, workLoad) private(j)
+		for(i = 1; i < gridSize-1; i++) {
+			for(j = 1; j < gridSize-1; j++) {
+				grid[i][j] = (new[i-1][j] +
+							  new[i+1][j] +
+							  new[i][j-1] +
+							  new[i][j+1]) * 0.25;
+			}
+		}
+		/*implicit barrier*/
+	}
+	
+	/*computation of the maximum difference*/
+	maxdiff = 0;
+	for(i = 1; i < gridSize-1; i++){
+		for(j = 1; j < gridSize-1; j++){
+			temp = grid[i][j] - new[i][j];
+			if(temp < 0)
+				temp = -temp;
+			if(temp > maxdiff)
+				maxdiff = temp;
+		}
+	}
+	/*print the maximum difference*/
+	printf("Maximum difference: %f\n", maxdiff);
+	
+	/*Prints the grid with boundary*/
+	for(i = 0; i < gridSize; i++){
+			printf("\n");
+		for(j = 0; j < gridSize; j++){
+			printf("%f  ", grid[i][j]);
+		}
+	}
 	
 	/*Behöver vi kanske köra free på vartenda element eller duger detta tror du?*/
 	free(grid);
 	free(new);
+	
+	printf("\n");
+	exit(0);
 }
